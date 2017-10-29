0b9e6201b59a8440d94f94a7d743764806f226ea
Lucas Cardozo <lucasecardozo@gmail.com>
2017-10-29 14:05:33 -0200

    initial commit

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.cocoen').cocoen();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
